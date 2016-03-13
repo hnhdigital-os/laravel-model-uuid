6183ef7d34c1db9d366b46d0beb2989a31e21b3a
Rocco Howard <rocco@bluora.com.au>
2016-03-13 00:29:17 -0500

    Applied fixes from StyleCI

--- src/UuidColumnTrait.php
+++ src/UuidColumnTrait.php
@@ -7,8 +7,9 @@ trait UuidColumnTrait
     /**
      * Cast an attribute to a native PHP type.
      *
-     * @param  string  $key
-     * @param  mixed  $value
+     * @param string $key
+     * @param mixed  $value
+     *
      * @return mixed
      */
     protected function castAttribute($key, $value)
@@ -19,18 +20,20 @@ trait UuidColumnTrait
 
         switch ($this->getCastType($key)) {
             case 'uuid':
-                $value = unpack("H*", $value);
-                return strtoupper(preg_replace("/([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12})/", "$1-$2-$3-$4-$5", $value[1]));
+                $value = unpack('H*', $value);
+
+                return strtoupper(preg_replace('/([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12})/', '$1-$2-$3-$4-$5', $value[1]));
             default:
                 return parent::castAttribute($key, $value);
         }
     }
 
     /**
-     * Lookup the model using a UUID
-     * 
-     * @param  string  $column
-     * @param  mixed  $value
+     * Lookup the model using a UUID.
+     *
+     * @param string $column
+     * @param mixed  $value
+     *
      * @return mixed
      */
     public static function whereUuid($column, $value)
@@ -39,10 +42,11 @@ trait UuidColumnTrait
     }
 
     /**
-     * Lookup the models using a UUID
-     * 
-     * @param  string  $column
-     * @param  mixed  $value
+     * Lookup the models using a UUID.
+     *
+     * @param string $column
+     * @param mixed  $value
+     *
      * @return mixed
      */
     public static function whereUuidIn($column, $value)
@@ -51,13 +55,13 @@ trait UuidColumnTrait
             $value = [$value];
         }
         if (count($value) == 0) {
-            return new static;
+            return new static();
         }
         $value = str_replace('-', '', $value);
         $value = preg_replace('/([a-zA-Z0-0].*)/', "UNHEX('$1')", $value);
         $value = implode(',', $value);
         $sql = sprintf("$column IN (%s)", $value);
+
         return parent::__callStatic('whereRaw', [$sql]);
     }
-
 }
